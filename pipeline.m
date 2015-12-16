619ddb3a141bcabab06828fe163bc3e896613621
Srivas Chennu <srivas@gmail.com>
2015-12-16 00:00:01 +0000

    Checked in Wed Dec 16 00:00:01 GMT 2015
    git-svn-id: file:///Users/chennu/Dropbox/Sources/AVSSR@16 480a7a08-7f5b-4c4a-a230-8927eaf6c0fb

--- pipeline.m
+++ pipeline.m
@@ -12,14 +12,13 @@ rejartifacts([basename '_clean'],2,4);
 rereference(basename,3);
 
 EEG = pop_loadset('filepath',filepath,'filename',[basename '.set']);
+
 fttest(EEG,36);
 fttest(EEG,78);
 fttest(EEG,44);
 fttest(EEG,84);
 
-fttest(EEG,'i1',[15 17])
-fttest(EEG,'i1all',[15 17])
-fttest(EEG,'i2',[19 21])
-fttest(EEG,'i2all',[19 21])
+fttest(EEG,[15 17])
+fttest(EEG,[19 21])
 
 end
